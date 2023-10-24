b5da7e89b34d2d4388b1ae3e182deebf2df102a8
SK ASIF ALI <58936617+SK-ASIF-ALI@users.noreply.github.com>
2023-10-25 01:46:17 +0530

    version 1.0

---
+++ depenedent.py
@@ -0,0 +1,3 @@
+
+def dep():
+    print("This is dependency file 1.0")
---
+++ test.py
@@ -0,0 +1,7 @@
+#version1.0
+import depenedent
+
+def app():
+    print("This is version 1.0")
+
+depenedent.dep() #new feature 1.0
